7731767fa8c9c74c08e789e84e8806ff67736941
Mena Aba <mena.tcoleman@gmail.com>
2022-12-08 20:57:00 -0800

    Added 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,16 @@
 #include<stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	/*String to be printed*/
+	/* String to be printed */
 	char firstString[] = "\"Programming is like building a multilingual puzzle";
 
-	/*Output string*/
+	/* Output string */
 	puts(firstString);
 
 	return (0);
