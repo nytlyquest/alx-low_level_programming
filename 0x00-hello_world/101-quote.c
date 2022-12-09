f054a074180cfa6b868836674eec0e322951a93d
Mena Aba <mena.tcoleman@gmail.com>
2022-12-08 21:59:43 -0800

    Added 101-quote.c & quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -8,6 +9,6 @@
 int main(void)
 {
 	/* printing to standard_error */
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
