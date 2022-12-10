f857bf4a2772b8fa6d19f5cb2b940d089b555303
Mena Aba <mena.tcoleman@gmail.com>
2022-12-09 19:58:41 -0800

    Added 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,5 +7,11 @@
  */
 int main(void)
 {
+	int baseTen;
+
+	for (baseTen = 0; baseTen < 10; baseTen++)
+		/* '0' + any digit will give you the digit in character form */
+		putchar('0' + baseTen);
+	putchar('\n');
 	return (0);
 }
