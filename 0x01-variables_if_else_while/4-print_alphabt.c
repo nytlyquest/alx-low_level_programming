f0996203f84dea3f6350c672d34dd454d1389a9b
Mena Aba <mena.tcoleman@gmail.com>
2022-12-09 19:34:29 -0800

    Added 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,5 +7,13 @@
  */
 int main(void)
 {
+	char alphabets;
+
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+	{
+		if (alphabets != 'e' && alphabets != 'q')
+			putchar(alphabets);
+	}
+	putchar('\n');
 	return (0);
 }
