e97052c6fbf9bd1d27cf496b33c58b5a8b2da24e
Mena Aba <mena.tcoleman@gmail.com>
2022-12-09 21:02:40 -0800

    Added 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,5 +7,17 @@
  */
 int main(void)
 {
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar('0' + num);
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
 	return (0);
 }
