a075cc971f74085bac1652c8470c6a1a51ef9bab
Mena Aba <mena.tcoleman@gmail.com>
2022-12-13 09:02:15 -0800

    Added 1-alphabet.c & updated main.h

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,9 @@
 #include "main.h"
 
+/**
+ * print_alphabet() - prints the alphabet in lower case
+ * @loAlpha: The variable holding earch alphabet through the loop
+ */
 void print_alphabet(void)
 {
 	char loAlpha;
