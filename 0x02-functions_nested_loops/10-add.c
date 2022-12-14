5ad388e44eb00f93c340c9135a8a369d17eb8a93
Mena Aba <mena.tcoleman@gmail.com>
2022-12-14 12:41:06 -0800

    10-add.c & updated main.h

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - prints the sum of 2 numbers
+ * @numOne: first number
+ * @numTwo: second number
+ *
+ * Description: printing should be done with _putchar function.
+ * Return: returns the sum of the numbers
+ */
+int add(int numOne, int numTwo)
+{
+	return (numOne + numTwo);
+}
