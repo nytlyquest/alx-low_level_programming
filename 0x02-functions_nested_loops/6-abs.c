3451b973d457e4455a2fb4a9e18a9353d72227ce
Mena Aba <mena.tcoleman@gmail.com>
2022-12-13 21:15:09 -0800

    Added 6-abs.c & updated main.h

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of a digit
+ * @abNum: the number whose absolute value needs to be given
+ *
+ * Description: printing should be done with _putchar function.
+ * Return: positive number of what is passed 
+ */
+int _abs(int abNum)
+{
+	if (abNum == 0)
+	{
+		return (0);
+	}
+	else if (abNum > 0)
+	{
+		return (abNum);
+	}
+	else
+	{
+		return (abNum * -1);
+	}
+}
