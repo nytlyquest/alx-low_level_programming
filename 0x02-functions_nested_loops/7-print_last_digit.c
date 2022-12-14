67455d65e883ec1e8a9cadddc43c7cb9cd7194a1
Mena Aba <mena.tcoleman@gmail.com>
2022-12-13 22:41:56 -0800

    Added 7-print_last_digit.c & updated main.h

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the digit of a number
+ * @theNum: the number whose last digit is yo be printed
+ *
+ * Description: printing should be done with _putchar function.
+ * Return: the number modulus 10.
+ */
+int print_last_digit(int theNum)
+{
+	if (theNum < 0)
+		theNum *= -1;
+	_putchar('0' + theNum%10);
+	return(theNum%10);
+}
