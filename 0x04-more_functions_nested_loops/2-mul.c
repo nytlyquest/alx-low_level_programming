84ec5f5a7cae8d15719ba077233d53d2e83bc406
Mena Aba <mena.tcoleman@gmail.com>
2022-12-15 14:35:12 -0800

    added 2-mul.c, updated main.h

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - calculates the product of 2 numbers
+ * @a: the first product number
+ * @b: the second product number
+ * 
+ * Description: numbers will be multiplied
+ * Return: product of the numbers
+ */
+int mul(int a, int b)
+{
+	int mulRslt;
+
+	mulRslt = a * b;
+	return(mulRslt);
+}
