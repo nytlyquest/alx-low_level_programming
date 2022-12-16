5a05bf7bf67e5c457c90f52ca4e212c3537c874a
Mena Aba <mena.tcoleman@gmail.com>
2022-12-15 17:48:52 -0800

    added 5-more_numbers.c, updated main.h

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints a range of numbers 10 times
+ *
+ * Description: numbers 0 through to 14 will be passed 
+ */
+void more_numbers (void)
+{
+	int dig, cnt = 0;
+
+	while (cnt < 10)
+	{
+		for (dig = 0; dig <= 14; dig++)
+		{
+			if (dig > 9)
+				_putchar('0' + (dig / 10));
+			_putchar('0' + (dig % 10));
+		}
+		_putchar('\n');
+		cnt++;
+	}
+}
