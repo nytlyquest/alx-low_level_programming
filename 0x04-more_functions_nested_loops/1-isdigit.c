9f96b0001e62dd6660cc204f81a9a46642988ae3
Mena Aba <mena.tcoleman@gmail.com>
2022-12-16 20:28:51 -0800

    added 1-isdigit.c, updated main

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,24 +1,22 @@
 #include "main.h"
 
 /**
- * _isdigit - checks for uppercase character
+ * _isdigit - a function that checks for digit 0 - 9
  * @c: the input to be checked
  *
  * Description: characters of the alphabet will be passed
- * Return: rtn is 1 if uppercase and 0 if otherwise
+ * Return: rtn is 1 if uppercase or 0 if otherwise
  */
 int _isdigit(int c)
 {
 	char dig;
-	int cdig, rtn = 0;
+	int rtn = 0;
 
-	for (dig = 0; dig <= 9; dig++)
+	for (dig = '0'; dig <= '9'; dig++)
 	{
-		for (cdig = '0'; cdig <= '9'; dig++)
-		{
-			if (c == dig || c == cdig)
-				rtn = 1;
-		}
+		if (c == dig)
+			rtn = 1;
 	}
+
 	return (rtn);
 }
