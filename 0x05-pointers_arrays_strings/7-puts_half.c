cb69ed6cad2e7b8b609b5dae810195c97a549da0
Mena Aba <mena.tcoleman@gmail.com>
2022-12-19 21:31:56 -0800

    added 7-puts_half.c, updated main.h

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,10 +12,9 @@ void puts_half(char *str)
 
 	while (str[strlen] != '\0')
 		strlen++;
-	strlen -= 1;
 	if (strlen % 2 == 1)
 	{
-		for (i = ((strlen + 1) / 2); i <= strlen; i++)
+		for (i = ((strlen / 2)); i <= strlen; i++)
 			_putchar(str[i]);
 	}
 	else
