f011116407bdf281789c959d01e6e63f157562b5
Mena Aba <mena.tcoleman@gmail.com>
2022-12-20 21:54:30 -0800

    added 5-rev_string.c, updated main.h

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - prints a string in reverse
+ * @s: pointer to string
+ *
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+	int cnt = i = 0;
+	char strchar;
+
+	while (s[cnt] != '\0')
+		cnt++;
+	while (i < cnt--)
+	{
+		strchar = s[i];
+		s[i++] = s[cnt];
+		s[cnt] = strchar;
+	}
+}
