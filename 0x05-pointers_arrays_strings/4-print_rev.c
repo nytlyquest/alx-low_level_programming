83383ba4a94c2ab9929b26bd7d2a52c5913b6685
Mena Aba <mena.tcoleman@gmail.com>
2022-12-20 20:51:07 -0800

    added 4-print_rev.c, updated main.h

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: pointer to string
+ *
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	int i, cnt = 0;
+
+	while (*s != '\0')
+	{
+		cnt++;
+		s++;
+	}
+	s -= cnt;
+	for (i = cnt; i >= 0; i--)
+		_putchar(*(s + i));
+	_putchar('\n');
+}
