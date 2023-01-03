df9b1d70d2eaa8471036c9ed6494072d5a184f2a
Mena Aba <mena.tcoleman@gmail.com>
2023-01-03 12:58:59 -0800

    added 1-memcpy.c, updated main.h

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memcpy - copiesbytes from one memory area to another
+ * @dest: destination memory area
+ * @src: source memory area
+ * @n: number of bytes to copy
+ *
+ * Return: return dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src [i];
+	return (dest);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -3,8 +3,8 @@
 * **0-memset.c:**
     * A function that fills the first n bytes of memory location pointed to by an array with a constant byte
 
-* **.c:**
-    *
+* **1-memcpy.c:**
+    * A function that copies the first n bytes from a source memory area to a destination memory area 
 
 * **.c:**
     *
