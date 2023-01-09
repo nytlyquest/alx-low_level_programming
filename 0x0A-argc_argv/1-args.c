865fff663cdef49132e395d6af390226f7351e55
Mena Aba <mena.tcoleman@gmail.com>
2023-01-09 06:10:00 -0800

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc: argument counter
+ * @argv: string array for arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -2,3 +2,6 @@
 
 * **0-whatsmyname.c:**
     * A program that prints it's name followed by a new line
+
+* **1-args.c:**
+    * A program that prints the number of arguments passed to it
