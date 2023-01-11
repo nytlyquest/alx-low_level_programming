f24b317306096789f58c431f47f07c03a702bef9
Mena Aba <mena.tcoleman@gmail.com>
2023-01-10 20:15:35 -0800

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - creates a duplicate of a string
+ * @str: pointer to string
+ *
+ * Return: pointer to duplicate string, else NULL
+ */
+char *_strdup(char *str)
+{
+	int cnt = 0, i = 0;
+	char *arr;
+
+	if (str[cnt] == '\0')
+		return (NULL);
+	while (str[cnt] != '\0')
+		cnt++;
+	arr = malloc(sizeof(*arr) * cnt);
+	while (i < cnt)
+	{
+		arr[i] = str[i];
+		i++;
+	}
+	return (arr);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -3,8 +3,8 @@
 * **0-create_array.c:**
     * A function that creates an array of chars and initialize it with a specified character
 
-* **.c:**
-    * 
+* **1-strdup.c:**
+    * A function that returns a pointer to a newly allocated space in memory, containing a copy of the string parameter`
 
 * **.c:**
     * 
