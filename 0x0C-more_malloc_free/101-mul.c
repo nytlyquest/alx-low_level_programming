9b9deb80e2dbd072ecaa195aecb65f2769b1ca15
Mena Aba <mena.tcoleman@gmail.com>
2023-01-13 21:58:34 -0800

    task 101

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int is_digit(char *s)
+{
+	int i = 0;
+	
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * main - check the code for ALX School studennts.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, prd;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		prd = num1 * num2;
+		printf("%d\n", result);
+		return (0);
+	}
+}
